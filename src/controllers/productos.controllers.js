6970013e5d5395c71dcc5690ba9984e954d8df38
Omar Mattos <omarmatt84@gmail.com>
2025-08-13 20:19:02 -0300

    podemos ver los datos que llegan en la solicitud para el producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -7,6 +7,15 @@ export const leerProductos = (req, res)=>{
 
 }
 
-export const crearProductos = (req, res)=>{
-
+export const crearProductos = async(req, res)=>{
+    try{
+        //1- Recibir el objeto que tengo que agregar a la BD
+        console.log(req.body)
+        //2- Validar los datos del objeto
+        //3- Guardar el objeto en la base de datos
+        
+        //4- Enviar respuesta
+    }catch(error){
+        console.error(error)
+    }
 }
