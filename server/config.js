fbdf4b20a51cbe7d069af7c177fe5d76846296c7
Omar Mattos <omarmatt84@gmail.com>
2025-08-19 21:05:48 -0300

    quite el console.log de prueba de crear producto

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import '../db/config.js'
+import mongoose from "../db/config.js";
 
 export default class Server {
   constructor() {
--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,3 +1,5 @@
+import Producto from "../models/producto.js";
+
 export const test = (req, res)=>{
     res.status(200);
     res.send('Primera prueba desde el backend')
@@ -10,12 +12,14 @@ export const leerProductos = (req, res)=>{
 export const crearProductos = async(req, res)=>{
     try{
         //1- Recibir el objeto que tengo que agregar a la BD
-        console.log(req.body)
         //2- Validar los datos del objeto
         //3- Guardar el objeto en la base de datos
-        
+        const nuevoProducto = new Producto(req.body);
+        await nuevoProducto.save();
         //4- Enviar respuesta
+        res.status(201).json({mensaje: 'El producto fue creado exitosamente'})
     }catch(error){
         console.error(error)
+        res.status(500).json({mensaje: 'Error al crear el producto'})
     }
 }
